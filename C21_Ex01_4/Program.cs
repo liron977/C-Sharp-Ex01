af7fe626b1795da7468e77393b7343a00bdeb0a0
Liron aviv Kotev <lironkt@mta.ac.il>
2021-07-21 23:19:04 +0300

    tar 4

--- C21_Ex01_4/Program.cs
+++ C21_Ex01_4/Program.cs
@@ -9,53 +9,88 @@ namespace C21_Ex01_4
     {
         static void Main()
         {
-
+            string userInput=getUserInput();
         }
 
-        private static int getUserInput()
+        private static void stringAnalysis()
+        {
+            string userInput = getUserInput();
+            int counterOfUppercaseLetters = 0;
+            if (char.IsLetter(userInput[0]))
+            {
+                printCountOfuppercaseLetter(userInput);
+            }
+            else
+            {
+                int.TryParse(userInput, out int decimalNumber);
+                PrintisNumberDivideBy5(decimalNumber);
+            }
+        }
+
+        private static void PrintisNumberDivideBy5(int i_number)
+        {
+            bool isDivideBy5 = false;
+            string str = "can";
+            isDivideBy5 = i_number % 5 == 0;
+            if (!isDivideBy5)
+            {
+                str = "can not";
+            }
+            string messageToTheUser =
+                String.Format($@"-The number {str} be divide by 5 ");
+            Console.WriteLine(messageToTheUser);
+        }
+
+        private static void printCountOfuppercaseLetter(string i_userInput)
+        {
+            int counterOfUppercaseLetters = 0;
+            for (int i = 0; i < i_userInput.Length; i++)
+            {
+                if (char.IsUpper(i_userInput[i]))
+                {
+                    counterOfUppercaseLetters++;
+                }
+            }
+
+            string messageToTheUser =
+                String.Format($@"-The number of uppercase letters is {counterOfUppercaseLetters}");
+            Console.WriteLine(messageToTheUser);
+        }
+        private static string getUserInput()
         {
             bool isValidInput = false;
             string userInput = string.Empty;
             while (!isValidInput)
             {
-                Console.WriteLine("Please enter the height of the sand machine:");
+                Console.WriteLine("Please enter a string:");
                 userInput = Console.ReadLine();
-                isValidInput = isConstantInput(userInput);
+                isValidInput = isValidString(userInput);
                 if (!isValidInput)
                 {
-                    Console.WriteLine("");
+                    Console.WriteLine("Invalid input,please enter a constant string with 8 chars ");
                 }
             }
             Console.Write(Environment.NewLine);
-            return int.Parse(userInput);
+            return userInput;
         }
 
         private static bool isValidString(string i_UserInput)
         {
-            char firstInputChar = i_UserInput[0];
             bool isValidInput = false;
-            if (firstInputChar >= 'a' && firstInputChar <= 'z')
+            char firstInputChar = i_UserInput[0];
+            bool isFirstCharIsDigit = char.IsDigit((firstInputChar));
+            bool isFirstCharIsLetter = char.IsLetter((firstInputChar));
+            if ((i_UserInput.Length == 8)&&(char.IsLetterOrDigit(firstInputChar)))
             {
-                isValidInput=isConstantInput(i_UserInput, 'a', 'z');
-            }
-            else if (firstInputChar >= 'A' && firstInputChar <= 'Z')
-            {
-                isValidInput= isConstantInput(i_UserInput, 'A', 'Z');
-            }
-            else if (firstInputChar >= '0' && firstInputChar <= '9')
-            {
-                isValidInput=isConstantInput(i_UserInput, '0', '9');
-            }
-
-            return isValidInput;
-        }
-        private static bool isConstantInput(string i_UserInput, char i_CharBeginningRange, char i_CharEndRange)
-        {
-            bool isValidInput = true;
-            for (int i = 0; i < i_UserInput.Length && isValidInput; i++)
-            {
-                isValidInput = i_UserInput[i] >= i_CharBeginningRange && i_UserInput[i] <= i_CharEndRange;
-
+                for (int i = 0; i < i_UserInput.Length; i++)
+                {
+                    isValidInput = ((isFirstCharIsLetter && char.IsLetter(i_UserInput[i])) ||
+                                   (isFirstCharIsDigit && char.IsDigit(i_UserInput[i])));
+                    if (!isValidInput)
+                    {
+                        break;
+                    }
+                }
             }
 
             return isValidInput;
