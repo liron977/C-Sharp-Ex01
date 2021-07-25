10f94a4084cdb76913f0ad8784c57191f1bf2052
Liron aviv Kotev <lironkt@mta.ac.il>
2021-07-25 11:13:59 +0300

    6

--- C21_Ex01_5/Program.cs
+++ C21_Ex01_5/Program.cs
@@ -7,21 +7,40 @@ namespace C21_Ex01_5
 {
     class Program
     {
-        public const int k_LengthString = 9;
+        public const int k_NumberOfDigit = 9;
 
         static void Main()
         {
-            getUserInput();
+            printNumberStatistics();
+        }
+
+        private static void printNumberStatistics()
+        {
+            string userInput = getUserInput();
+            int maxDigit = findMaximumDigit(userInput);
+            int minDigit = findMinimumDigit(userInput);
+            int countDividedBy3Digit = FindCountDigitDividedBy3(userInput);
+            int countDigitsBiggerFromFirstDigit=FindCountDigitsBiggerFromFirstDigit(userInput);
+            string messageToTheUserAboutTheNumbers = String.Format($@"-The maximum digit is {maxDigit}.
+-The minimum digit is {minDigit}.
+-The count of digits that can be divided by 3: {countDividedBy3Digit}. 
+-There are {countDigitsBiggerFromFirstDigit} digits which are bigger then the first digit.");
+           
+            Console.WriteLine(messageToTheUserAboutTheNumbers);
 
         }
 
         private static string getUserInput()
         {
-            string userInputStr;
+            Console.WriteLine("Please enter a positive number with 9 digits");
+            string userInputStr=String.Empty;
+
             userInputStr = Console.ReadLine();
             while (!isValidInput(userInputStr))
             {
+                Console.WriteLine("Invalid input,please enter a positive number with 9 digits");
                 userInputStr = Console.ReadLine();
+              
             }
 
             return userInputStr;
@@ -29,50 +48,55 @@ namespace C21_Ex01_5
 
         private static int findMinimumDigit(string i_UserInputStr)
         {
-            int minimumDigit = 0;
-            for (int i = 0; i < k_LengthString; i++)
+            double minimumDigit = char.GetNumericValue(i_UserInputStr[0]);
+
+            for (int i = 0; i < k_NumberOfDigit; i++)
             {
-                if (minimumDigit > i_UserInputStr[i])
-                {
-                    minimumDigit = i_UserInputStr[i];
-                }
+               
+                    minimumDigit =Math.Min(char.GetNumericValue(i_UserInputStr[i]), minimumDigit);
             }
 
-            return minimumDigit;
+            return (int)minimumDigit;
         }
 
         private static int findMaximumDigit(string i_UserInputStr)
         {
-            int MaximumDigit = 0;
-            for (int i = 0; i < k_LengthString; i++)
+            double MaximumDigit =0;
+            for (int i = 0; i < k_NumberOfDigit; i++)
             {
-                if (MaximumDigit > i_UserInputStr[i])
+                MaximumDigit = Math.Max(char.GetNumericValue(i_UserInputStr[i]), MaximumDigit);
+            }
+
+            return (int)MaximumDigit;
+        }
+
+        private static int FindCountDigitDividedBy3(string i_UserInputStr)
+        {
+            int countDividedBy3Digit = 0;
+            for (int i = 0; i < k_NumberOfDigit; i++)
+            {
+                if (char.GetNumericValue(i_UserInputStr[i]) % 3 == 0)
                 {
-                    MaximumDigit = i_UserInputStr[i];
+                    countDividedBy3Digit++;
                 }
             }
 
-            return MaximumDigit;
+            return countDividedBy3Digit;
+
         }
-
-        /*  private static int howManyDigitCanBeDivideBy3(string i_UserInputStr)
-          {
-  
-          }*/
-
 
         private static bool isValidInput(string i_UserInputStr)
         {
             bool isUserValidInput = true;
 
-            if (i_UserInputStr.Length != k_LengthString)
+            if (i_UserInputStr.Length != k_NumberOfDigit)
             {
                 isUserValidInput = false;
             }
 
             else
             {
-                for (int i = 0; i < k_LengthString; i++)
+                for (int i = 0; i < k_NumberOfDigit; i++)
                 {
                     if (!char.IsDigit(i_UserInputStr[i]))
                     {
@@ -86,23 +110,23 @@ namespace C21_Ex01_5
 
         }
 
-        private static int convertBinaryNumberToDecimal(string i_UserInput)
+        private static int FindCountDigitsBiggerFromFirstDigit(string i_UserInput)
         {
-            int pow = 0;
-            double decimalNumber = 0;
-            int number = 0;
-
-            int.TryParse(i_UserInput, out int binaryNumber);
-
-            while (binaryNumber > 0)
+            int countDigitsBiggerFromFirstDigit = 0;
+            int firstDigit = (int)char.GetNumericValue(i_UserInput[0]);
+            int currDigit = 0;
+            for (int i = 1; i < k_NumberOfDigit; i++)
             {
-                decimalNumber += (binaryNumber % 10) * Math.Pow(2, pow);
-                pow++;
-                binaryNumber = binaryNumber / 10;
+                currDigit = (int) char.GetNumericValue(i_UserInput[i]);
+                if (currDigit > firstDigit)
+                {
+                    countDigitsBiggerFromFirstDigit++;
+                }
             }
 
-            return (int) decimalNumber;
+            return countDigitsBiggerFromFirstDigit;
         }
+
     }
 }
-/*   }*/
+
